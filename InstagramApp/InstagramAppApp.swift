777271c69b21469757a14162faa019aa9c6a82c2
Vergil <aloss8411@gmail.com>
2022-06-20 21:58:40 +0800

    Initial Commit

---
+++ InstagramApp/InstagramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramAppApp.swift
+//  InstagramApp
+//
+//  Created by Lan Ran on 2022/6/20.
+//
+
+import SwiftUI
+
+@main
+struct InstagramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
